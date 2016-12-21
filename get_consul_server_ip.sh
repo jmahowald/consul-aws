7f255c1d525253b8e0a2b87200ab3d36c64b420c
Josh Mahowald <joshua.mahowald@gmail.com>
2016-12-21 17:28:07 -0600

    Stupid bash with stupid whitespace

--- get_consul_server_ip.sh
+++ get_consul_server_ip.sh
@@ -1,10 +1,10 @@
-if [ -z ${CONSUL_SERVER_IP+x}] then
-
-if [ -z ${CONSUL_HTTP_URL+x}] then
-  echo "You must set either the CONSUL_SERVER_IP or the CONSUL_HTTP_URL" 
-  exit -1
-fi
-
+if [ -z ${CONSUL_SERVER_IP+x}] 
+then
+  if [ -z ${CONSUL_HTTP_URL+x}] 
+  then
+    echo "You must set either the CONSUL_SERVER_IP or the CONSUL_HTTP_URL" 
+    exit -1
+  fi
 CONSUL_SERVER_IP=$(curl -q  $CONSUL_HTTP_URL/v1/catalog/service/consul  \
   | jq '.[0].Address')
 fi
